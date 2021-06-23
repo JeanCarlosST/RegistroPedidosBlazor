eb23b9ed6ae180a12b4e9f5c3b3364886c874987
Kleyton72 <jeanc.1582@gmail.com>
2021-06-23 19:48:06 -0400

    Agregando nueva migración

--- Migrations/20210623233250_CambiandoRelaciones.Designer.cs
+++ Migrations/20210623233250_CambiandoRelaciones.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RegistroPedidosBlazor.DAL;
 
 namespace RegistroPedidosBlazor.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20210623233250_CambiandoRelaciones")]
+    partial class CambiandoRelaciones
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20210623233250_CambiandoRelaciones.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RegistroPedidosBlazor.Migrations
+{
+    public partial class CambiandoRelaciones : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_OrdenesDetalle_ProductoID",
+                table: "OrdenesDetalle",
+                column: "ProductoID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrdenesDetalle_Productos_ProductoID",
+                table: "OrdenesDetalle",
+                column: "ProductoID",
+                principalTable: "Productos",
+                principalColumn: "ProductoID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrdenesDetalle_Productos_ProductoID",
+                table: "OrdenesDetalle");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrdenesDetalle_ProductoID",
+                table: "OrdenesDetalle");
+        }
+    }
+}
